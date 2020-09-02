f44a44bd37771826784fcd9e94d3654f233d4841
Irina Sergeevna <Hiddekel1047@yandex.ru>
2020-09-03 01:00:23 +0300

    feat: открыть форму по классу

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+
+const popup = document.querySelector('.popup');
+
+const popupOpenButton = document.querySelector('.profile__edit-button');
+
+const popupCloseButton = document.querySelector('.popup__close');
+
+console.log({popup, popupOpenButton, popupCloseButton});
+
+const popupToggle = function() {
+    popup.classList.toggle('popup_is-opened');
+}
+
+popupToggle();
